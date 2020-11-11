88dc9c3c13801aa8722712236ab7ad24b3da13fa
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2020-11-11 13:39:14 -0300

    Adição das funções de marcar e retirar uma tarefa

--- js/script.js
+++ js/script.js
@@ -3,22 +3,45 @@ let itens = 0
 
 $adicionar.addEventListener('click', ()=>{
     const $h2 = document.querySelector('h2')
-   const $textoTarefa = document.getElementById('nova-tarefa')
-   const $listaTarefasItens = document.getElementById('lista-tarefas-itens')
+    const $textoTarefa = document.getElementById('nova-tarefa')
+    const $listaTarefasItens = document.getElementById('lista-tarefas-itens')
 
-    $h2.innerText = `${++itens} items`
-   $listaTarefasItens.innerHTML += gerarItem($textoTarefa.value)
+    if($textoTarefa.value != ''){
+        $listaTarefasItens.innerHTML += gerarItem($textoTarefa.value)
+        $h2.innerText = `${++itens} tarefa(s)`
+    }
 })
 
 
 function gerarItem(texto){
     return (
-    `<li>
-        <div>${texto}</div>
-        <div>
-            <button class="completo">✔️</button>
-            <button class="retirar">❌</button>
-        </div>
-    </li>`
+        `<li>
+            <div>${texto}</div>
+            <div>
+                <button class="completo" onclick="marcarCompleto(this)">✔️</button>
+                <button class="retirar" onclick="retirarElemento(this)">❌</button>
+            </div>
+        </li>`
     )
+}
+
+
+function marcarCompleto(that){
+    let $elementoPai = that.parentNode
+    $elementoPai = $elementoPai.parentNode
+
+    let conteudoDoLI = $elementoPai.querySelector('div')
+    conteudoDoLI.style.cssText = 'color: green'
+}
+
+function retirarElemento(that){
+    let $elementoPai = that.parentNode
+    $elementoPai = $elementoPai.parentNode
+    
+    let $li = $elementoPai
+    let $ul = $elementoPai.parentNode
+    $ul.removeChild($li)
+
+    const $h2 = document.querySelector('h2')
+    $h2.innerText = (itens-1 == 0) ? 'Lista Vazia...' : `${--itens} tarefa(s)`
 }
